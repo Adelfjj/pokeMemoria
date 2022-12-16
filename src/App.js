248c5e95157225bbc84e202d41018588f5857043
Adelfjj <adelsonfranciscojj@outlook.com>
2022-12-16 19:21:40 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect} from "react";
+import Card from "./components/layout/Card";
 
 function App() {
+
+  const [pokemons,setPokemons] = useState([]);
+
+ useEffect(() => {
+   const urls = getRandomIntInclusive();
+   const fetchData = async () => {
+    try {
+     var response = await Promise.all(
+       urls.map(url => fetch(url)
+        .then(res => res.json()))
+        )
+    } catch (error){
+      console.log("Error",error)
+    }
+    setPokemons(response);
+  }
+  fetchData();
+  },[])
+  
+
+  const handleClick =(cardContainer) =>{
+    cardContainer.current.firstChild.classList.toggle("flip");
+  }
+
+  const getRandomIntInclusive = () => {
+    const numPokemons = 139;
+    const result = Math.floor(Math.random() * numPokemons);
+
+    var endpoints =[];
+
+    for(let i=result;i<result+12;i++){
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    }
+    return endpoints;
+  }
+
+  console.log(pokemons)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="appHeader">
+       <h1>Jogo da memória POKEMON</h1>
       </header>
+      <main>
+        <section>
+          {pokemons.length > 0 &&
+            pokemons.map((pokemon) => 
+             <Card handleClick={handleClick}
+             pokemon={pokemon} key={pokemon.id}/> 
+            )
+          }
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/components/layout/Card.js
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+import "./Card.css";
+
+
+function Card ({handleClick, pokemon}){
+    const cardContainer = useRef(null);
+
+    const clickCard = (e) => {
+        e.preventDefault();
+        handleClick(cardContainer);
+    }
+    return(
+        <div className="mainContainer" ref={cardContainer}>
+            <div className="theCard" onClick={clickCard}>
+                <div className="theFront">
+                    <p>front</p>
+                </div>
+                <div className="theBack">
+                    <img src={pokemon.sprites.other.home.front_default} alt={pokemon.name}/>
+                    <div>
+                        <h2>{pokemon.name}</h2>
+                        {pokemon.types.length > 0 && 
+                            pokemon.types.map((pok,key) => {
+                            return <p key={key}>{pok.type.name}</p>
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
